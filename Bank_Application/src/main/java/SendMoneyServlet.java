95fc5931085c86738c91e02cd4d80e5ecc504b80
Tirumala4585 <150008283+Tirumala4585@users.noreply.github.com>
2024-08-16 00:03:03 +0530

    Update SendMoneyServlet.java

--- Bank_Application/src/main/java/SendMoneyServlet.java
+++ Bank_Application/src/main/java/SendMoneyServlet.java
@@ -29,31 +29,69 @@ public class SendMoneyServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		int accountId = Integer.parseInt(request.getParameter("account_id"));
-		double amount = Double.parseDouble((String)request.getParameter("amount"));
-		//String fromAccountNo = request.getParameter("account_no");
-		String toAccountNo = request.getParameter("recipient_account");
-		response.setContentType("text/html");
-    	PrintWriter out = response.getWriter();
-		BankDAO dao = new BankDAO();
-		BankDTO dto = dao.getBalance(accountId);
-	    double balance	= dto.getCurrentBalance();
-	    balance=balance-amount;
-	    dto.setCurrentBalance(balance);
-	    	TransactionDTO transdto = new TransactionDTO();
-	    	transdto.setTransactionId(accountId);
-	    	//transdto.setSourceAccountNo(fromAccountNo);
-	    	transdto.setTargetAccountNo(toAccountNo);
-	    	transdto.setAmount(amount);
-	    	if(dao.updateBalance(dto,accountId) && dao.setTransactionDetails(transdto))
-		    { 
-		    	out.print("Transaction successfull");
-		    }
-		    else 
-		    {
-		    	out.print("Transaction is failed");
-		    }
-	    
-	    
+             	HttpSession session=request.getSession(false);
+		if(session!=null) {
+		   UserDTO user = (UserDTO)session.getAttribute("user");
+		   int userId = user.getUserId();
+		   int sourceAccountId = Integer.parseInt(request.getParameter("account_id"));
+		   String sourceAccountNo = request.getParameter("account_no");
+		   String targetAccountNo = request.getParameter("target_account");
+		   double amount = Double.parseDouble((String)request.getParameter("amount"));
+		   
+		   
+		   response.setContentType("text/html");
+    	   PrintWriter out = response.getWriter();
+    	   
+		   BankDAO dao = new BankDAO();
+		   double sourceBalance = dao.getBalance(sourceAccountId);
+		   
+		   
+	       if(sourceBalance>=amount)
+	       {	   
+	           sourceBalance=sourceBalance-amount;
+	           int targetAccountId= dao.getAccountId(targetAccountNo);
+	           double targetBalance = dao.getBalance(targetAccountId);
+	           targetBalance = targetBalance+amount;
+	           if(dao.updateBalance(sourceBalance, sourceAccountId)&&dao.updateBalance(targetBalance, targetAccountId))
+	           {
+	              TransactionDTO sourcetransdto = new TransactionDTO();  
+	              sourcetransdto.setSourceAccountNo(sourceAccountNo);
+		          sourcetransdto.setTargetAccountNo(targetAccountNo);
+		          sourcetransdto.setTransactionType("Debit");
+		          sourcetransdto.setAmount(amount);
+		          sourcetransdto.setBalance(dao.getBalance(sourceAccountId));
+		          
+		          
+		          TransactionDTO targettransdto = new TransactionDTO();
+		          targettransdto.setSourceAccountNo(targetAccountNo);
+		          targettransdto.setTargetAccountNo(sourceAccountNo);
+		          targettransdto.setTransactionType("Credit");
+		          targettransdto.setAmount(amount);
+		          targettransdto.setBalance(dao.getBalance(targetAccountId));
+		          if(dao.setTransactionDetails(sourcetransdto)&&dao.setTransactionDetails(targettransdto))
+		          {
+		        	 BankDAO bankdao = new BankDAO();
+		        	 List<BankDTO> acclist = bankdao.getAccountDetails(userId);
+		        	 request.setAttribute("accountdetails",acclist);
+                     RequestDispatcher rd = request.getRequestDispatcher("Home.jsp"); 
+                     rd.forward(request, response);
+		          } 
+		          else {
+		        	  out.println("Transaction iS Failed");
+		          }
+	           }
+	           else{
+	        	   out.println("Transaction is Failed");
+		        	  
+		          }
+	       }      
+	     else {
+	    	 out.println("Insufficient Balance");
+	     }
+      }
+	else {
+		response.sendRedirect("Login.jsp");
+	}
+		
 	}
 }
