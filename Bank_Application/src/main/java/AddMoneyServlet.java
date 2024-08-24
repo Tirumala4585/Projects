fdca4077f7ec6e5c6a0464bd13ab5bfc09ee4595
Tirumala4585 <150008283+Tirumala4585@users.noreply.github.com>
2024-08-24 23:52:14 +0530

    Update AddMoneyServlet.java

--- Bank_Application/src/main/java/AddMoneyServlet.java
+++ Bank_Application/src/main/java/AddMoneyServlet.java
@@ -30,29 +30,50 @@ public class AddMoneyServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		int accountId = Integer.parseInt(request.getParameter("accountId"));
-		double balance = Double.parseDouble(request.getParameter("amount"));
-		response.setContentType("text/html");
+	 HttpSession session = request.getSession(false);
+		if(session!=null) {
+		UserDTO user = (UserDTO)session.getAttribute("user");
+		int userId = user.getUserId();
+		int accountId = Integer.parseInt(request.getParameter("account_Id"));
+		double amount = Double.parseDouble(request.getParameter("amount"));
+		String accountNo=request.getParameter("account_no");
+		System.out.println(accountNo);
+		
 		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
 		BankDAO dao = new BankDAO();
-		BankDTO dto = dao.getBalance(accountId);
-		double current_balance=dto.getCurrentBalance();
-		current_balance=current_balance+balance;
-		dto.setCurrentBalance(current_balance);
-		TransactionDTO transdto = new TransactionDTO();
-    	transdto.setTransactionId(accountId);
-    	//transdto.setSourceAccountNo(fromAccountNo);
-    	transdto.setAmount(balance);
-		if(dao.updateBalance(dto,accountId) && dao.setTransactionDetails(transdto))
+		double current_balance=dao.getBalance(accountId);
+		current_balance=current_balance+amount;
+		if(dao.updateBalance(current_balance,accountId))
 		{
-			response.sendRedirect("Home.jsp");
-		}
+		    TransactionDTO transdto = new TransactionDTO();
+    	            transdto.setSourceAccountNo(accountNo);
+    	            transdto.setTargetAccountNo(accountNo);
+    	            transdto.setTransactionType("Credit");
+    	            transdto.setAmount(amount);
+    	            transdto.setBalance(dao.getBalance(accountId));
+    	            transdto.setAccountId(accountId);
+		    if(dao.setTransactionDetails(transdto))
+		    {
+			   BankDAO bankdao = new BankDAO();
+	    	           List<BankDTO> acclist = bankdao.getAccountDetails(userId);
+	    	           request.setAttribute("accountdetails", acclist);
+			   RequestDispatcher rd = request.getRequestDispatcher("Home.jsp");
+			   rd.forward(request,response);
+		    }
+		    else {
+		    	out.println("Balance not sucessfully not updated");
+		    }
+		}   
 		else {
+			
 			out.print("Balance not successfully not updated");
 		}
-			
-		}
+	 }
+	else {	
+		response.sendRedirect("Login.jsp");
+	}	
 		
-	}
+   }
 
-
+}
