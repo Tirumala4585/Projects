a2f75d26acd475fd669e7619ddde017123ae2955
Tirumala4585 <150008283+Tirumala4585@users.noreply.github.com>
2024-08-25 23:01:28 +0530

    Update AddAccountServlet.java

--- Bank_Application/src/main/java/AddAccountServlet.java
+++ Bank_Application/src/main/java/AddAccountServlet.java
@@ -26,31 +26,49 @@ public class AddAccountServlet extends HttpServlet {
         
     }
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session = request.getSession(false);
-		UserDTO user = (UserDTO)session.getAttribute("user");
-		String userName=user.getUserName();
-		String accountNo=request.getParameter("account_no");
-		String bankName=request.getParameter("bank_name");
-		String ifscCode=request.getParameter("ifsc_code");
-		String accountType=request.getParameter("account_type");
-		double currBal=Double.parseDouble(request.getParameter("intial_bal"));
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		BankDAO dao = new BankDAO();
-		BankDTO dto = new BankDTO();
-		dto.setUserName(userName);
-		dto.setAccountNo(accountNo);
-	    dto.setBankName(bankName);
-	    dto.setIfscCode(ifscCode);
-	    dto.setAccountType(accountType);
-	    dto.setCurrentBalance(currBal);
-	    if(dao.setAccountDetails(dto)) {
-		    response.sendRedirect("http://localhost:8081/Bank_Application/Home.jsp");
-	    }
-	    else
-	    {
-	    	out.print("Your Accoutn details are not Added");
-	    }
+	HttpSession session = request.getSession(false);
+		if(session!=null) {
+		  UserDTO user = (UserDTO)session.getAttribute("user");
+		  if(user!=null) {
+		    int userId=user.getUserId();
+		    String userName=user.getUserName();
+		    String accountNo=request.getParameter("account_no");
+		    String bankName=request.getParameter("bank_name");
+		    String ifscCode=request.getParameter("ifsc_code");
+		    String accountType=request.getParameter("account_type");
+		    double currBal=Double.parseDouble(request.getParameter("intial_bal"));
+		    response.setContentType("text/html");
+		    PrintWriter out = response.getWriter();
+		    
+		    BankDAO dao = new BankDAO();
+		    BankDTO dto = new BankDTO();
+		    dto.setUserId(userId);
+		    dto.setUserName(userName);
+		    dto.setAccountNo(accountNo);
+	            dto.setBankName(bankName);
+	            dto.setIfscCode(ifscCode);
+	            dto.setAccountType(accountType);
+	            dto.setCurrentBalance(currBal);
+	            if(dao.setAccountDetails(dto)) {
+		          BankDAO detailsdao = new BankDAO();
+		          List<BankDTO> acclist = detailsdao.getAccountDetails(userId);
+		          request.setAttribute("accountdetails", acclist);
+		          RequestDispatcher rd = request.getRequestDispatcher("Home.jsp");
+		          rd.forward(request,response);
+	            }
+	            else {
+	    	       out.print("Your Account details are not added");
+	    	       RequestDispatcher rd = request.getRequestDispatcher("AddAccount.jsp");
+	    	       rd.include(request, response);
+	            }
+		 }
+		 else {
+			response.sendRedirect("Login.jsp");
+		  }
+		}
+		else {
+			response.sendRedirect("Login.jsp");
+		}	
 	}
 
 }
